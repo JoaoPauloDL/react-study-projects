b39ba940040422e63edbc075641653a172783258
João Paulo <joaopaulomayka@gmail.com>
2024-03-22 21:24:14 -0300

    feat: implement the handleMultiSelection function
    allows that when pressing the Enable Multi Selection button the corresponding answers to the questions will be displayed without being hidden after clicking on another question

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -5,25 +5,45 @@ import "./styles.css";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnablebleMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   const handleSingleSelection = (getCurrentID) => {
     setSelected(getCurrentID === selected ? null : getCurrentID);
   };
 
+  const handleMultiSelection = (getCurrentID) => {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentID = cpyMultiple.indexOf(getCurrentID)
+
+    console.log(findIndexOfCurrentID);
+    if(findIndexOfCurrentID === -1) cpyMultiple.push(getCurrentID)
+    else cpyMultiple.splice(findIndexOfCurrentID, 1)
+
+    setMultiple(cpyMultiple)
+  };
+  console.log(selected,multiple)
   return (
     <div className="wrapper">
+      <button onClick={() => setEnablebleMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((item) => (
             <div className="item">
               <div
-                onClick={() => handleSingleSelection(item.id)}
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(item.id)
+                    : () => handleSingleSelection(item.id)
+                }
                 className="title"
               >
                 <h3>{item.question}</h3>
                 <span className="arrow">+</span>
               </div>
-              {selected === item.id ? (
+              {selected === item.id || multiple.indexOf(item.id) !== -1 ? (
                 <div className="content">
                   <p>{item.answer}</p>
                 </div>
