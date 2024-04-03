26f4261b2f03ee84ae3acddb2080f9b72b0b5f2c
João Paulo <joaopaulomayka@gmail.com>
2024-04-03 18:13:13 -0300

    feat: Implement functionality to load more products
    - Adds functionality to load more products when clicking on the "Load More Products" button
    - Updates the state of products to display the new products
    - Adjusts the product count to request the next set of products from the server

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <RandomColor/>
       <StarRating/>
       <ImageSlider url = {'https://picsum.photos/v2/list'} page={'1'} limit={10}/>
+      <LoadMoreData/>
     </div>
   );
 }
