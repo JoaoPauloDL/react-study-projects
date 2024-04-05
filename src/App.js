be957588412f96e917da7b150435d1429921d44d
João Paulo <joaopaulomayka@gmail.com>
2024-04-04 22:26:00 -0300

    feat(TreeView): Add TreeView component
    - Implements the TreeView component to display hierarchical menus

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import "./App.css";
+import Accordion from "./components/accordion";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
-import './App.css';
-import Accordion from './components/accordion';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
 
 function App() {
-
-  
-
   return (
     <div className="App">
-      <Accordion/>
-      <RandomColor/>
-      <StarRating/>
-      <ImageSlider url = {'https://picsum.photos/v2/list'} page={'1'} limit={10}/>
-      <LoadMoreData/>
+      <Accordion />
+      <RandomColor />
+      <StarRating />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={10}
+      />
+      <LoadMoreData />
+      <TreeView menus={menus} />
     </div>
   );
 }
