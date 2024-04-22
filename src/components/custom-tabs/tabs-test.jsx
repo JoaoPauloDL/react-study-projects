679de7de5a9e869132457144001dd7e6d0de3a0d
João Paulo <joaopaulomayka@gmail.com>
2024-04-22 18:07:26 -0300

    feat: Add TabTest component
    Adds the TabTest component to demonstrate the usage of the Tabs component.
    
    - Implements the TabTest component to showcase the Tabs component functionality.
    - Defines tabs with labels and content for demonstration purposes.
    - Uses the Tabs component with the provided tabs and handleChange function.
    - Logs the current tab index when a tab is changed.
    
    This commit adds the TabTest component, which can be used as a demonstration of the Tabs component and its functionality.
    
    Closes #2

---
+++ src/components/custom-tabs/tabs-test.jsx
@@ -0,0 +1,36 @@
+
+import Tabs from './tabs'
+
+const TabTest = () => {
+
+  const RandomComponent = () => {
+    return <h1>Some random content</h1>
+  }
+
+  const tabs = [
+    {
+        label : 'Tab 1',
+        content : <div>This is content for Tab 1</div>
+    },
+    {
+        label : 'Tab 2',
+        content : <div>This is content for Tab 2</div>
+    },
+    {
+        label : 'Tab 3',
+        content: <RandomComponent/>
+    }
+  ];
+
+  const handleChange = (currentTabIndex) => {
+    console.log(currentTabIndex)
+  }
+
+  return (
+    <div>
+        <Tabs tabsContent={tabs} onChange={handleChange}/>
+    </div>
+  )
+}
+
+export default TabTest
