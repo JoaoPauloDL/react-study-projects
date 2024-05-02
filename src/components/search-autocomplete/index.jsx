9ace427acdb1d43c73b69b280912f6c81f024839
João Paulo <joaopaulomayka@gmail.com>
2024-05-02 18:56:24 -0300

    feat: Add handleChange and handleClick methods to search autocomplete
    Add handleChange method to handle changes in the search input field.
    This method updates the searchParam state with the value entered by the user
    and filters the users based on the entered text.
    
    Add handleClick method to handle clicks on items in the autocomplete dropdown.
    This method hides the dropdown, sets the searchParam state to the text of the clicked item,
    and clears the filteredUsers state.

--- src/components/search-autocomplete/index.jsx
+++ src/components/search-autocomplete/index.jsx
@@ -1,49 +1,79 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import Suggestions from "./suggestions";
 
 const SearchAutocomplete = () => {
-
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
-  const [searchParam, setSearchParam] = useState('')
- 
+  const [searchParam, setSearchParam] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredUsers, setFilteresUsers] = useState([]);
+
+  const handleChange = (event) => {
+    // Obtém o valor digitado no campo de input e converte para minúsculas
+    const query = event.target.value.toLowerCase();
+
+    // Atualiza o estado 'searchParam' com o valor digitado
+    setSearchParam(query);
+    if (query.length > 1) {
+      // Filtra os usuários com base na entrada do usuário
+      const filteredData =
+        users && users.length
+          ? users.filter((item) => item.toLowerCase().indexOf(query) > -1)
+          : [];
+      setFilteresUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  };
+
+  const handleClick = (event) =>{
+    setShowDropdown(false)
+    setSearchParam(event.target.innerText);
+    setFilteresUsers([])
+  }
+
   const fetchListOfUsers = async () => {
     try {
-        setLoading(true)
-       const response = await fetch('https://dummyjson.com/users');
-       const data = await response.json();
-       
-       if(data && data.users && data.users.length){
+      setLoading(true);
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+
+      if (data && data.users && data.users.length) {
         // Usando map para extrair os primeiros nomes dos usuários
         setUsers(data.users.map((item) => item.firstName));
         setLoading(false);
-        setError(null)
-       }
-    
-       
-
-    }catch(error) {
-        setLoading(false)
-        console.log(error);
-        setError(error)
+        setError(null);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      setError(error);
     }
-  }
-
-
+  };
 
   useEffect(() => {
     fetchListOfUsers();
-  }, [])
+  }, []);
 
   return (
     <div className="search-autocomplete-container">
-      <input
-        type="text"
-        name="search-users"
-        placeholder="Search Users here..."
-      />
+      {loading ? (
+        <h1>Loading Data! Please wait</h1>
+      ) : (
+        <input
+          value={searchParam}
+          type="text"
+          name="search-users"
+          placeholder="Search Users here..."
+          onChange={handleChange}
+        />
+      )}
+
+      {showDropdown && <Suggestions handleClick={handleClick} data={filteredUsers} />}
     </div>
   );
 };
